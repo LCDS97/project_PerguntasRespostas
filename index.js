4c4da80259d46f5b075a6a6ab2987032c168f116
LCDS97 <lucas.cds1997@gmail.com>
2021-03-22 20:36:21 -0300

    UPDATE PROJETO

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+// Bibliotecas a ser utilizada
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+// Utilizando o framework ejs para back-end do Projeto
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
+// Utilizando bodyParser para traduzir em js dados do formulário
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas do Projeto
 app.get("/",(req, res) => {
     res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req, res) => {
+        res.send("Formulario enviado!")
 });
 
 
